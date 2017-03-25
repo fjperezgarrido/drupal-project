b0eed63bdd9c57cf848488fef4c55fb9414323d7
paco <fjperezgarrido@gmail.com>
2017-03-25 13:32:29 +0100

    actualizamos el test

--- web/modules/custom/one_two/tests/src/Functional/BrowserTest.php
+++ web/modules/custom/one_two/tests/src/Functional/BrowserTest.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Drupal\Tests\one_two\Functional;
+
 use Drupal\Tests\BrowserTestBase;
 
 /**
@@ -10,8 +11,30 @@ use Drupal\Tests\BrowserTestBase;
 */
 class BrowserTest extends BrowserTestBase {
 
-  function testDrupalGet() {
-    $this->drupalGet('user/register');
-    $this->assertSession()->pageTextContains( 'Create new account');
+  protected $user;
+
+  public static $modules = ['block', 'node', 'datetime'];
+
+  protected function setUp() {
+    parent::setUp();
+    $this->drupalCreateContentType([ 'type' => 'page', 'name' => 'Basic page']);
+    $this->user = $this->drupalCreateUser([ 'edit own page content', 'create page content' ]);
+    $this->drupalPlaceBlock( 'local_tasks_block');
+  }
+
+  function testNodeCreate() {
+    $this->drupalLogin($this->user);
+    $title = $this->randomString();
+    $body = $this->randomString( 32);
+    $edit = [
+      'Title' => $title,
+      'Body' => $body,
+      ];
+    $this->drupalPostForm( 'node/add/page', $edit, t('Save'));
+
+    $node = $this->drupalGetNodeByTitle($title);
+    $this->assertTrue($node);
+    $this->assertEquals($title, $node->getTitle());
+    $this->assertEquals($body, $node->body->value);
   }
 }
