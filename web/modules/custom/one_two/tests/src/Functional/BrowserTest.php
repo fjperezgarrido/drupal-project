4100a805b1a20483b038aeecefe16ce21ad2c4ff
paco <fjperezgarrido@gmail.com>
2017-03-25 15:36:11 +0100

    actualizamos file para phpsniffer

--- web/modules/custom/one_two/tests/src/Functional/BrowserTest.php
+++ web/modules/custom/one_two/tests/src/Functional/BrowserTest.php
@@ -17,20 +17,20 @@ class BrowserTest extends BrowserTestBase {
 
   protected function setUp() {
     parent::setUp();
-    $this->drupalCreateContentType([ 'type' => 'page', 'name' => 'Basic page']);
-    $this->user = $this->drupalCreateUser([ 'edit own page content', 'create page content' ]);
-    $this->drupalPlaceBlock( 'local_tasks_block');
+    $this->drupalCreateContentType(['type' => 'page', 'name' => 'Basic page']);
+    $this->user = $this->drupalCreateUser(['edit own page content', 'create page content' ]);
+    $this->drupalPlaceBlock('local_tasks_block');
   }
 
   function testNodeCreate() {
     $this->drupalLogin($this->user);
     $title = $this->randomString();
-    $body = $this->randomString( 32);
+    $body = $this->randomString(32);
     $edit = [
       'Title' => $title,
       'Body' => $body,
-      ];
-    $this->drupalPostForm( 'node/add/page', $edit, t('Save'));
+    ];
+    $this->drupalPostForm('node/add/page', $edit, t('Save'));
 
     $node = $this->drupalGetNodeByTitle($title);
     $this->assertTrue($node);
